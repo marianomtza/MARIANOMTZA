5607c6943012c88b30c45942f970b33d8079dd4d
Mariano Martínez <mariano1302mariano1302@gmail.com>
2026-04-21 22:31:58 -0600

    Auto-sync: 2026-04-21 22:31:58

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AudioProvider } from './contexts/AudioContext'
+import { MotionProvider } from './contexts/MotionContext'
 import { ThemeProvider } from './contexts/ThemeContext'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AudioProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <MotionProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </MotionProvider>
     </AudioProvider>
   </React.StrictMode>
 )
