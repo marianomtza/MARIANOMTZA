0e0e807d6f4617b84f4e193f63de83c1cfe4b0c0
Mariano Martínez <mariano1302mariano1302@gmail.com>
2026-04-23 00:24:53 -0600

    ch 2

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Roster } from './Roster'
 export { default as Booking } from './Booking'
 export { default as Hero } from './Hero'
+export { default as DrawingCanvas } from './DrawingCanvas'
